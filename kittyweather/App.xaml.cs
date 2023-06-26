037617ee77f2784e6c5a081082fecc2637e47e63
Piotr Jelonek <piotrek.jelonek@icloud.com>
2023-06-26 15:42:08 +0200

    Initial commit

---
+++ kittyweather/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace kittyweather;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ kittyweather/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace kittyweather;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
