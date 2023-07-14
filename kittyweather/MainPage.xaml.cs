628b8bb78f843519e0f369f2bb26e30870184b4b
Piotr Jelonek <piotrek.jelonek@icloud.com>
2023-07-14 19:49:11 +0200

    Added label with text

--- kittyweather/MainPage.xaml.cs
+++ kittyweather/MainPage.xaml.cs
@@ -6,15 +6,4 @@ public partial class MainPage : ContentPage {
     public MainPage() {
         InitializeComponent();
     }
-
-    private void OnCounterClicked(object sender, EventArgs e) {
-        count++;
-
-        if (count == 1)
-            CounterBtn.Text = $"Clicked {count} time";
-        else
-            CounterBtn.Text = $"Clicked {count} times";
-
-        SemanticScreenReader.Announce(CounterBtn.Text);
-    }
 }
