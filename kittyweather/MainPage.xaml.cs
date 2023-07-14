70259ba9e0e722678722f1239ba81241c0c3cc59
Piotr Jelonek <piotrek.jelonek@icloud.com>
2023-07-14 20:05:02 +0200

    Added "RefreshData" button and random temp generator

--- kittyweather/MainPage.xaml.cs
+++ kittyweather/MainPage.xaml.cs
@@ -1,9 +1,16 @@
 ﻿namespace kittyweather;
 
 public partial class MainPage : ContentPage {
-    int count = 0;
+    private readonly Random _random = new Random();
 
     public MainPage() {
         InitializeComponent();
+    }
+
+    private void RefreshTemperature(object sender, EventArgs e) {
+        int temp = _random.Next(-20, 40);
+        string text = "Current temperature = " + temp + "°C";
+
+        TemperatureText.Text = text;
     }
 }
