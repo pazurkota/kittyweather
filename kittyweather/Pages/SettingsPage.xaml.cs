570f02fedf2a77bd4ffef5c8aba4e0343fe0f117
Piotr Jelonek <piotrek.jelonek@icloud.com>
2023-07-24 18:54:51 +0200

    Added basic API Key settings functionality

--- kittyweather/Pages/SettingsPage.xaml.cs
+++ kittyweather/Pages/SettingsPage.xaml.cs
@@ -1,13 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 
 namespace kittyweather.Pages; 
 
 public partial class SettingsPage : ContentPage {
     public SettingsPage() {
         InitializeComponent();
+    }
+    
+    private async void OnEntryCompleted(object sender, EventArgs e) {
+        await DisplayAlert("Settings", "Api Key successfully saved!", "OK");
+        apiKeyLabel.Text = $"API Key: {((Entry)sender).Text}";
+        
+        Preferences.Set("apikey", $"{((Entry)sender).Text}");
     }
 }
