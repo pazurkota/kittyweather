8245a1edbfe41adb0b6ce437b89dabf5a293e0f9
Piotr Jelonek <piotrek.jelonek@icloud.com>
2023-07-24 23:01:10 +0200

    Created method to get data from API

--- kittyweather/Data/ApiService.cs
+++ kittyweather/Data/ApiService.cs
@@ -1,5 +1,30 @@
-﻿namespace kittyweather.Data; 
+﻿using RestSharp;
 
-public class ApiService {
+namespace kittyweather.Data;
+
+interface IApiService {
+    Task<Weather> GetWeather(string cityName);
+}
+
+public class ApiService : IApiService {
+    private readonly string BASEURL = "https://api.weatherapi.com/v1/";
     
+    public async Task<Weather> GetWeather(string cityName) {
+        string apiKey = Preferences.Get("apiKey", "");
+
+        if (string.IsNullOrEmpty(apiKey)) {
+            throw new UnauthorizedAccessException("API Key is not set!");
+        }
+        
+        var options = new RestClientOptions(BASEURL) {
+            ThrowOnAnyError = true
+        };
+        
+        var client = new RestClient(options);
+        var request = new RestRequest($"forecast.json?key={apiKey}&q={cityName}&aqi=yes&alerts=yes&days=2");
+        
+        var response = await client.GetAsync<Weather>(request);
+
+        return response;
+    }
 }
