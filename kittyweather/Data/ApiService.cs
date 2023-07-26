b6b4b4fa0b6a11924149eb9e649ceeaab154d53c
Piotr Jelonek <piotrek.jelonek@icloud.com>
2023-07-26 21:51:04 +0200

    Revert "Minor changes in ApiService.cs"
    This reverts commit c0f4035535fd3a53dce4db989827d9d08295d7c5.

--- kittyweather/Data/ApiService.cs
+++ kittyweather/Data/ApiService.cs
@@ -4,13 +4,13 @@ using Newtonsoft.Json;
 namespace kittyweather.Data;
 
 interface IApiService {
-    Task<Weather> GetWeather(string cityName);
+    Weather GetWeather(string cityName);
 }
 
 public class ApiService : IApiService {
     private readonly string BASEURL = "https://api.weatherapi.com/v1/";
     
-    public async Task<Weather> GetWeather(string cityName) {
+    public Weather GetWeather(string cityName) {
         string apiKey = Preferences.Get("apiKey", null);
 
         if (string.IsNullOrEmpty(apiKey)) {
@@ -24,20 +24,13 @@ public class ApiService : IApiService {
         var client = new RestClient(options);
         var request = new RestRequest($"forecast.json?key={apiKey}&q={cityName}&aqi=yes&alerts=yes&days=2");
 
-        var response = await client.ExecuteAsync(request);
-        var content = response.Content;
-
-        if (string.IsNullOrEmpty(content))
-        {
-            throw new ArgumentNullException("JSON Content returnes null!");
-        }
-
-        var weather = JsonConvert.DeserializeObject<Weather>(content);
-
+        var response = client.ExecuteAsync(request).Result.Content;
+        var weather = JsonConvert.DeserializeObject<Weather>(response);
+        
         return weather;
     }
     
-    public async Task<Weather> GetWeather(double latitude, double longitude) {
+    public Weather GetWeather(double latitude, double longitude) {
         string apiKey = Preferences.Get("apiKey", "");
 
         if (string.IsNullOrEmpty(apiKey)) {
@@ -50,15 +43,9 @@ public class ApiService : IApiService {
         
         var client = new RestClient(options);
         var request = new RestRequest($"forecast.json?key={apiKey}&q={latitude},{longitude}&aqi=yes&alerts=yes&days=2");
-
-        var response = await client.ExecuteAsync(request);
-        var content = response.Content;
-
-        if (string.IsNullOrEmpty(content)) {
-            throw new ArgumentNullException("JSON Content returnes null!");
-        }
-
-        var weather = JsonConvert.DeserializeObject<Weather>(content);
+        
+        var response = client.ExecuteAsync(request).Result.Content;
+        var weather = JsonConvert.DeserializeObject<Weather>(response);
         
         return weather;
     }
