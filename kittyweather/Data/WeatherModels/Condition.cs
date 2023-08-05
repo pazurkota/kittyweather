e080d972a33d481c5b71c906da0d17a44f672570
Piotr Jelonek <piotrek.jelonek@icloud.com>
2023-08-05 20:44:18 +0200

    Added test icons

--- kittyweather/Data/WeatherModels/Condition.cs
+++ kittyweather/Data/WeatherModels/Condition.cs
@@ -4,5 +4,6 @@ namespace kittyweather.Data;
 
 public class Condition {
     [JsonProperty("text")] public string ConditionState { get; set; } = null!;
+    [JsonProperty("icon")] public string ConditionIcon { get; set; }
     [JsonProperty("code")] public int ConditionCode { get; set; }
 }
