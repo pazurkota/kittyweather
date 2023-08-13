04239bb275116df7c8f939b791440151324eac88
⭐PazurK0TA <piotrek.jelonek@icloud.com>
2023-08-13 19:44:24 +0200

    Added functionality to api key buttons

--- kittyweather/Pages/SettingsPage.xaml.cs
+++ kittyweather/Pages/SettingsPage.xaml.cs
@@ -8,7 +8,31 @@ public partial class SettingsPage : ContentPage {
     }
     
     private async void OnEntryCompleted(object sender, EventArgs e) {
-        await DisplayAlert("Settings", "Api Key successfully saved!", "OK");
+        await DisplayAlert("Info", "API Key successfully saved!", "OK");
         Preferences.Set("apiKey", $"{((Entry)sender).Text}");
+    }
+
+    private async void RevealApiKey(object sender, EventArgs e) {
+        var apiKey = Preferences.Get("apiKey", null);
+
+        if (apiKey != null) {
+            ApiKeyLabel.Text = apiKey;
+            ApiKeyLabel.IsVisible = true;
+        }
+        else {
+            await DisplayAlert("Error", "Cannot reveal API Key: key is not set!", "OK");
+            ApiKeyLabel.IsVisible = false;
+        }
+    }
+
+    private async void CopyApiKey(object sender, EventArgs e) {
+        var apiKey = Preferences.Get("apiKey", null);
+
+        if (apiKey != null) {
+            await Clipboard.Default.SetTextAsync(apiKey);
+        }
+        else {
+            await DisplayAlert("Error", "Cannot copy API Key: key is not set!", "OK");
+        }
     }
 }
