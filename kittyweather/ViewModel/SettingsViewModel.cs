7dcc064b3da54fe6fa64b05712873a1e731ff029
⭐PazurK0TA <piotrek.jelonek@icloud.com>
2023-08-14 16:53:16 +0200

    Binded pickers in SettingsPage.xaml

---
+++ kittyweather/ViewModel/SettingsViewModel.cs
@@ -0,0 +1,30 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace kittyweather.ViewModel; 
+
+public partial class SettingsViewModel : ObservableObject {
+    [ObservableProperty] private string selectedTemperatureUnit;
+    [ObservableProperty] private string selectedVisibilityUnit;
+    [ObservableProperty] private string selectedAirPressureUnit;
+    [ObservableProperty] private string selectedPrecipitationUnit;
+
+    public List<string> TemperatureOptions { get; } = new() {
+        "Celsius",
+        "Fahrenheit"
+    };
+
+    public List<string> VisibilityOptions { get; } = new() {
+        "Kilometers",
+        "Miles"
+    };
+
+    public List<string> AirPressureOptions { get; } = new() {
+        "Millibars",
+        "Inches"
+    };
+
+    public List<string> PrecipitationOptions { get; } = new() {
+        "Millimeters",
+        "Inches"
+    };
+}
