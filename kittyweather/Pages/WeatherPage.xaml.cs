67158f4d0d906c31941a62ef9d8fb8d7e122b2c9
Piotr Jelonek <piotrek.jelonek@icloud.com>
2023-08-18 22:02:28 +0200

    Implemented city search bar

--- kittyweather/Pages/WeatherPage.xaml.cs
+++ kittyweather/Pages/WeatherPage.xaml.cs
@@ -40,8 +40,7 @@ public partial class WeatherPage : ContentPage {
         }
         catch (UnauthorizedAccessException) {
             await DisplayAlert("Error", "Unable to download data: Please set the API Key in settings.", "OK");
-        }
-        catch (Exception) {
+        } catch (Exception) {
             await DisplayAlert("Error", "Unable to download data: Please try again later.", "OK");
         }   
     }
@@ -50,5 +49,36 @@ public partial class WeatherPage : ContentPage {
         var location = await Geolocation.GetLocationAsync();
         latitude = location.Latitude;
         longitude = location.Longitude;
+    }
+
+    private async void SearchCity(object sender, EventArgs e)
+    {
+        try {
+            var city = ((Entry)sender).Text;
+
+            var viewModel = (WeatherViewModel)BindingContext;
+            await viewModel.GetWeatherData(city);
+
+            viewModel.GetUvIndexDescription();
+            viewModel.GetHourlyWeather();
+
+            viewModel.GetTemperature();
+            viewModel.GetVisibility();
+            viewModel.GetAirPressure();
+            viewModel.GetPrecipitation();
+
+            var alert = viewModel.Weather.Alerts.WeatherAlerts.FirstOrDefault();
+
+            if (alert != null) {
+                WeatherAlertBox.IsVisible = true;
+                WeatherAlertDesc.Text = alert.AlertHeadline;
+            } else {
+                WeatherAlertBox.IsVisible = false;
+            }
+        } catch (UnauthorizedAccessException) {
+            await DisplayAlert("Error", "Unable to download data: Please set the API Key in settings.", "OK");
+        } catch (Exception) {
+            await DisplayAlert("Error", "Unable to download data: Please try again later.", "OK");
+        }
     }
 }
