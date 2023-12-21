c1b5aebf76860b487f71f1291c98cd8387389b42
Piotr Jelonek <piotrek.jelonek@icloud.com>
2023-12-21 13:57:02 +0100

    Removed configs for MacCatalyst, Tizen and Windows

--- kittyweather/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,8 +0,0 @@
-﻿using Foundation;
-
-namespace kittyweather;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate {
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
