a806741fa69e9aacf413eed2b0ce64699e773d86
Piotr Jelonek <piotrek.jelonek@icloud.com>
2024-01-06 12:56:16 +0100

    Modified WelcomePage
    added field to enter api key and moving to AppShell

--- kittyweather/Pages/WelcomePage.xaml.cs
+++ kittyweather/Pages/WelcomePage.xaml.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using kittyweather.Data;
 
 namespace kittyweather.Pages;
 
@@ -11,6 +7,24 @@ public partial class WelcomePage : ContentPage
     public WelcomePage()
     {
         InitializeComponent();
+    }
+
+    async void SetApiKey_OnClicked(object sender, EventArgs e)
+    {
+        string apiKey = ((Entry)sender).Text;
+        bool isValid = await ApiService.CheckApiKey(apiKey);
+        
+        if (isValid)
+        {
+            Preferences.Set("apiKey", apiKey);
+            await DisplayAlert("Info", "API Key successfully saved!", "OK");
+
+            await Navigation.PushModalAsync(new AppShell { FlyoutBehavior = FlyoutBehavior.Disabled });
+        }
+        else
+        {
+            await DisplayAlert("Error", "API Key is invalid!", "OK");
+        }
     }
 }
 
